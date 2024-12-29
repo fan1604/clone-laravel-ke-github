85cb29423675fea8257aee28d6c91a9c383257e9
fan1604 <fanzr1604@gmail.com>
2024-12-29 08:23:57 +0700

    halo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,117 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <title>Portofolio Moh.Affan Romdlani</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <h1>halaman home</h1>
+
+   <!--Navbar-->
+   <nav>
+    <div class="wrapper">
+        <!--logo-->
+        <div class="logo"><img src="img/AN01.png"></div>
+        <!--menu-->
+        <div class="menu"> 
+            <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="#tentang-saya">Tentang saya</a></li>
+            <li><a href="#kontak">Kontak</a></li>
+            </ul>
+        </div>
+    </div>
+   </nav>
+   <!--Navbar end-->
+
+   <!--Hero / jumbotron-->
+   <header>
+    <div class="header-opacity"></div>
+    <div class="header-jumbotron">
+        <h4>Moh.Affan Romdlani <br>IST2204039</h4>
+        <h5>Universitas Annuqayah</h5>
+        <p>Web Portofolio Mahasiswa #UAS_Semester_IV</p>
+        <a href="http://www.youtube.com/@mohaffanromdlani" class="button button-x1">Mulai jelajah Youtube </a>
+        </div>
+    </div>
+   </header>
+   <!--Hero / jumbotron end-->
+
+   <!--Statistic-->
+   <section id="statistic">
+    <div class="wrapper">
+        <div>
+            <p><i class="fa-solid fa-gear"> 0</i></p>
+            <small>Years Experient</small>
+        </div>
+        <div>
+            <p><i class="fa-solid fa-user-graduate"> 0</i></p>
+            <small>Student</small>
+        </div>
+        <div>
+            <p><i class="fa-brands fa-youtube"> 0</i></p>
+            <small>Happy Subscribers</small>
+        </div>
+    </div>
+   </section>
+   <!--statistic end-->
+
+    <!--Tentang saya-->
+   <section id="tentang-saya">
+    <div class="wrapper">
+        <h4>Tentang Saya</h4>
+        <p>Hello, I am a student at Annuqayah University, information technology study.</p>
+        <div>
+            <div class="foto"><img src="img/profile1.png"/></div>
+         <div class="desc"> 
+            <p>A confident and creative person who is self-motivated, independent and comes to you with a strong background in digital media. Detail oriented, and have a more or less comprehensive understanding of digital science. Illustrator and the latest web technology. Having an attitude of getting the job done without sacrificing quality,
+                a hard-working and ambitious individual will have no trouble following instructions from seniors and customers.</p>
+        <div class="sosial">
+            <a href="https://www.instagram.com/a_fan_1604?igsh=MWxtc29mcmI2M3dxYg=="><i class="fa-brands fa-instagram"></i></a>
+            <a href=""><i class="fa-brands fa-github"></i></a>
+            <a href="https://www.tiktok.com/@fanfan161004?_t=8oa6KU5iUpG&_r=1"><i class="fa-brands fa-tiktok"></i></a>
+            <a href="http://www.youtube.com/@mohaffanromdlani"><i class="fa-brands fa-youtube"></i></a>
+        </div>
+        </div>
+    </div>
+    </div>
+   </section>
+
+   <!--Kontak-->
+   <section id="kontak">
+    <div class="wrapper">
+        <h4>Kontak</h4>
+        <p>Anda dapat terhubung dengan developer.</p>
+    <div class="kontak">
+    <div class="infokontak">
+
+    <div class="box">
+        <div class="icon"><i class="fa-solid fa-location-dot"></i></div>
+        <div class="text">
+            <h3>Alamat</h3>
+            <p>Jalan Balai Desa Aeng Panas <br>RT.3/RW.3 Dusun Galis <br>Aeng Panas,Pragaan,Sumenep,69465</p>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="icon"><i class="fa-brands fa-whatsapp"></i></div>
+        <div class="text">
+            <h3>Whatsapp</h3>
+            <p>+62 838-4996-6990</p>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="icon"><i class="fa-regular fa-envelope"></i></div>
+        <div class="text">
+            <h3>Email</h3>
+            <p>mohaffanromdlani@gmail.com</p>
+        </div>
+    </div>
+    
+    </div>
+    </div>
+    </div>   
+   </section>
 </body>
 </html>
